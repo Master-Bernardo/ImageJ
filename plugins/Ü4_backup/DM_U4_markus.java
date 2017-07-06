7dd298ec35dc9e5d885af3513405d688a5948567
markuswestphal <markus.westphal@googlemail.com>
2017-07-07 01:47:16 +0200

    add backup file to ImageJ>plugins>Ü4_backup

---
+++ plugins/Ü4_backup/DM_U4_markus.java
@@ -0,0 +1,284 @@
+package digitalmedia.Übung4solo;
+
+import ij.*;
+import ij.io.*;
+import ij.process.*;
+import ij.gui.*;
+import ij.plugin.filter.*;
+
+public class DM_U4 implements PlugInFilter {
+
+	// Felder, ImagePlus und ein String[] zur Auswahl der Methoden
+	protected ImagePlus imp;
+	final static String[] choices = { "Wischen", "Weiche Blende", "Overlay", "Schieb-Blende", "Chroma-Keying", "Eigene Überblendung"};
+
+	public int setup(String arg, ImagePlus imp) {
+		this.imp = imp;
+		return DOES_RGB + STACK_REQUIRED;
+	}
+
+	public static void main(String args[]) {
+		ImageJ ij = new ImageJ(); // neue ImageJ Instanz starten und anzeigen
+		ij.exitWhenQuitting(true);
+
+		IJ.open("/Applications/ImageJ/Bilder/StackB.zip");
+
+		DM_U4 sd = new DM_U4();
+		sd.imp = IJ.getImage();
+		ImageProcessor B_ip = sd.imp.getProcessor();
+		sd.run(B_ip);
+	}
+
+	public void run(ImageProcessor B_ip) {
+		// Film B wird uebergeben
+		ImageStack stack_B = imp.getStack();
+
+		int length = stack_B.getSize();
+		int width = B_ip.getWidth();
+		int height = B_ip.getHeight();
+
+		// ermoeglicht das Laden eines Bildes / Films
+		Opener o = new Opener();
+		OpenDialog od_A = new OpenDialog("Auswählen des 2. Filmes ...", "");
+
+		// Film A wird dazugeladen
+		String dateiA = od_A.getFileName();
+		if (dateiA == null)
+			return; // Abbruch
+		String pfadA = od_A.getDirectory();
+		ImagePlus A = o.openImage(pfadA, dateiA);
+		if (A == null)
+			return; // Abbruch
+
+		ImageProcessor A_ip = A.getProcessor();
+		ImageStack stack_A = A.getStack();
+
+		if (A_ip.getWidth() != width || A_ip.getHeight() != height) {
+			IJ.showMessage("Fehler", "Bildgrößen passen nicht zusammen");
+			return;
+		}
+
+		// Neuen Film (Stack) "Erg" mit der kleineren Laenge von beiden erzeugen
+		length = Math.min(length, stack_A.getSize());
+
+		ImagePlus Erg = NewImage.createRGBImage("Ergebnis", width, height, length, NewImage.FILL_BLACK);
+		ImageStack stack_Erg = Erg.getStack();
+
+		// Dialog fuer Auswahl des Ueberlagerungsmodus
+		GenericDialog gd = new GenericDialog("Überlagerung");
+		gd.addChoice("Methode", choices, "");
+		gd.showDialog();
+
+		int methode = 0;
+		String s = gd.getNextChoice();
+		if (s.equals("Wischen"))
+			methode = 1;
+		if (s.equals("Weiche Blende"))
+			methode = 2;
+		if (s.equals("Overlay"))
+			methode = 3;
+		if (s.equals("Schieb-Blende"))
+			methode = 4;
+		if (s.equals("Chroma-Keying"))
+			methode = 5;
+		if (s.equals("Eigene Überblendung"))
+			methode = 6;
+
+		// Arrays fuer die einzelnen Bilder
+		int[] pixels_B;
+		int[] pixels_A;
+		int[] pixels_Erg;
+
+		// Schleife ueber alle Bilder
+		for (int z = 1; z <= length; z++) {
+			pixels_B = (int[]) stack_B.getPixels(z);
+			pixels_A = (int[]) stack_A.getPixels(z);
+			pixels_Erg = (int[]) stack_Erg.getPixels(z);
+
+			int pos = 0;
+			for (int y = 0; y < height; y++)
+				for (int x = 0; x < width; x++, pos++) {
+					int cA = pixels_A[pos];
+					int rA = (cA & 0xff0000) >> 16;
+					int gA = (cA & 0x00ff00) >> 8;
+					int bA = (cA & 0x0000ff);
+
+					int cB = pixels_B[pos];
+					int rB = (cB & 0xff0000) >> 16;
+					int gB = (cB & 0x00ff00) >> 8;
+					int bB = (cB & 0x0000ff);
+
+					// WISCHEN ************************************************************
+					if (methode == 1) {
+						if (y + 1 > (z - 1) * (double) width / (length - 1)) {
+							pixels_Erg[pos] = pixels_B[pos];
+						} else {
+							pixels_Erg[pos] = pixels_A[pos];
+						}
+					}
+
+					// WEICHE BLENDE ******************************************************
+					if (methode == 2) {
+						/*
+						 * cartoon style should be correct according to my
+						 * calculations- dont know why it looks like ths
+						 */
+
+						int r = (rA / length * (z - 1) + rB / length * (length - (z - 1)));
+						int g = (gA / length * (z - 1) + gB / length * (length - (z - 1)));
+						int b = (bA / length * (z - 1) + bB / length * (length - (z - 1)));
+						pixels_Erg[pos] = 0xFF000000 + ((r & 0xff) << 16) + ((g & 0xff) << 8) + (b & 0xff);
+					}
+
+					// OVERLAY ************************************************************
+					if (methode == 3) {
+						int r;
+						int g; 
+						int b;
+						r = overlay(rA, rB);
+						g = overlay(gA, gB);
+						b = overlay(bA, bB);
+
+						pixels_Erg[pos] = 0xFF000000 + ((r & 0xff) << 16) + ((g & 0xff) << 8) + ( b & 0xff);
+					}
+					
+					// SCHIEB-BLENDE ******************************************************
+					if (methode == 4) {
+						int limit =  (width-1) * (z-1) / (length-1);
+						int r; 
+						int g;
+						int b;
+						int deltaX = x - limit;
+						
+						if (deltaX >= 0) {
+							int newPos = y * width + deltaX;
+							r = (pixels_B[newPos] >> 16) & 0xff;
+							g = (pixels_B[newPos] >>  8) & 0xff;
+							b = (pixels_B[newPos] >>  0) & 0xff;	
+						} else {
+							int newPos = y * width + ((width) + deltaX);
+							r = (pixels_A[newPos] >> 16) & 0xff;
+							g = (pixels_A[newPos] >>  8) & 0xff;
+							b = (pixels_A[newPos] >>  0) & 0xff;
+						}
+						
+						pixels_Erg[pos] = 0xFF000000 + ((r & 0xff) << 16) + ((g & 0xff) << 8) + ( b & 0xff);
+					}
+					
+					// CHROMA-KEYING ******************************************************
+					if (methode == 5) {
+						//TrenngraphGraph = y= x +70  -> drüber ist transparent, drunter ist sichtbar . im CbCr Farbraum
+						
+						//alle Pixel sind erstmal wie im ersten Bild
+						int r = rB;
+						int g = gB;
+						int b = bB;
+						
+						//dann nehme ich mir die Pixel vom 2 Bild und wandle sie in YCbCr um
+						int YCbCr[] =  RGB2YCbCr(rA,gA,bA);
+						
+						//if(Cb+10 > Cr) - wenn dies y,x Werte unter dem Trenngraphen liegen
+						if(YCbCr[1]+10 > YCbCr[2]){
+							r=rA;
+							g=gA;
+							b=bA;
+							//Übergangszone, zweiter Graph
+						}else if(YCbCr[1]+10> YCbCr[2]&&YCbCr[1]+20 < YCbCr[2]){
+							r=rA/2+rB/2;
+							g=gA/2+gB/2;
+							b=bA/2+bB/2;
+						
+						}
+						pixels_Erg[pos] = 0xFF000000 + ((r & 0xff) << 16) + ((g & 0xff) << 8) + ( b & 0xff);
+					}
+					
+					// EIGENE ÜBERBLEDUNG *************************************************
+					if (methode == 6) {
+						// Extra kreisblende ( vielleicht mehrere Kreisblenden hintereinander- Wasserwellen nach einem Tropfen)
+						int r1b = (int) Math.sqrt(Math.pow(width, 2)+Math.pow(height, 2))/2; //radius - so groß wie die diagonale/2
+						//Mittelpunkt
+						int xMitte = width/2;
+						int yMitte = height/2;
+						int r,g,b;
+						
+						int deltaX = x - xMitte;
+						int deltaY = y - yMitte;
+						//ändere den Radius
+						//System.out.println("length: "+ length);
+						//System.out.println("z = " + z);
+						//System.out.println("r1b " + r1b);
+						double cu = r1b/length;
+						int r1 = (int) (cu*z*6);
+						int r2 = (int) (cu*z*0.75*6);
+						int r3 = (int) (cu*z*0.5*6);
+						int r4 = (int) (cu*z*0.25*6);
+						//150/95*95 =95?!!
+						//System.out.println("r1 " +r1);
+						if (Math.pow(deltaX, 2) + Math.pow(deltaY, 2) < Math.pow(r4, 2)){
+							r = rB;
+							g = gB;
+							b = bB;
+						}else if (Math.pow(deltaX, 2) + Math.pow(deltaY, 2) < Math.pow(r3, 2)){
+							r=(int) ((0.75)*rB+(0.25)*rA);
+							g=(int) ((0.75)*gB+(0.25)*gA);
+							b=(int) ((0.75)*bB+(0.25)*bA);
+						}else if (Math.pow(deltaX, 2) + Math.pow(deltaY, 2) < Math.pow(r2, 2)){
+							r=(int) ((0.5)*rB+(0.5)*rA);
+							g=(int) ((0.5)*gB+(0.5)*gA);
+							b=(int) ((0.5)*bB+(0.5)*bA);
+						}else if (Math.pow(deltaX, 2) + Math.pow(deltaY, 2) < Math.pow(r1, 2)){
+							r=(int) ((0.25)*rB+(0.75)*rA);
+							g=(int) ((0.25)*gB+(0.75)*gA);
+							b=(int) ((0.25)*bB+(0.75)*bA);
+							
+						}else{
+							r=rA;
+							g=gA;
+							b=bA;
+						}
+
+						pixels_Erg[pos] = 0xFF000000 + ((r & 0xff) << 16) + ((g & 0xff) << 8) + ( b & 0xff);
+					}
+
+				}
+		}
+
+		// neues Bild anzeigen
+		Erg.show();
+		Erg.updateAndDraw();
+
+	}
+	
+	public static int[] RGB2YCbCr(double r, double g, double b){
+		int[] result = new int[3];
+		//Y
+		result[0]= (int) (0.299*r+0.587*g+0.114*b);
+		//Cb
+		result[1]= (int) (-0.168736*r-0.331264*g+0.5*b);
+		//Cr
+		result[2]= (int) (0.5*r-0.418688*g-0.081312*b);
+		return result;
+	}
+	
+	public static int[] YCbCr2RGB(double y, double cb, double cr){
+		int[] result = new int[3];
+		//r
+		result[0]=(int) (y+1.402*cr);
+		//g
+		result[1]=(int) (y-0.3441*cb-0.7141*cr);
+		//b
+		result[2]=(int) (y+1.772*cb);
+		return result;
+	}
+
+	private int overlay(int colA, int colB) {
+		int r;
+		if(colB <= 128) {
+			 r = (colA * colB) / 128;
+		} else {
+			 r = 255 - ((255 - colA) * (255 - colB) / 128);
+		}
+		return r;
+	}
+	
+}
